ce6d58a16918ee79bf88fb68cb4d3ae1772d57a6
IshaKapadia <202012046@daiict.ac.in>
2021-06-22 11:54:08 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/routes.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
@@ -18,12 +19,13 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.deepPurple,
           // primaryTextTheme: GoogleFonts.latoTextTheme()
           fontFamily: GoogleFonts.lato().fontFamily),
+      // debugShowCheckedModeBanner: false,
       darkTheme: ThemeData(brightness: Brightness.dark),
       initialRoute: "/",
       routes: {
         "/": (context) => LoginPage(),
-        "/home": (context) => HomePage(),
-        "/Login": (context) => LoginPage()
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.LoginRoute: (context) => LoginPage()
       },
     );
   }
